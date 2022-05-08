6a4701136dfab832070e00ab7300281c98d1e8cf
Diya Pathak <dpathak@usc.edu>
2022-05-08 16:07:46 -0700

    Mymatches (still needs debugging) - but still compiles properly

--- Frontend/src/Components/Matches/Matches.js
+++ Frontend/src/Components/Matches/Matches.js
@@ -8,6 +8,11 @@ import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import LogoutIcon from '@mui/icons-material/Logout';
 import Badge from '@mui/material/Badge';
 import PublicIcon from '@mui/icons-material/Public';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function Matches(){
 
@@ -16,6 +21,51 @@ function Matches(){
         //clear data first??
 
         return navigate('/');
+    }
+    function SimpleAccordion() {
+        return (
+            <div>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography>Accordion 2</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion disabled>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel3a-content"
+                  id="panel3a-header"
+                >
+                  <Typography>Disabled Accordion</Typography>
+                </AccordionSummary>
+              </Accordion>
+            </div>
+          );
     }
 
     return(
@@ -48,11 +98,12 @@ function Matches(){
                     < PublicIcon fontSize="large"/>
                     </Badge>
                </li>
-            
+
+
+
            <li id="logo_li">
                <Link to="/" style={{ textDecoration: 'none' }}>
                    {/* <img src={logo} id="logo_img" alt="logo" /> */}
-                   〰️
                </Link>
            </li>
            </ul>
@@ -60,11 +111,13 @@ function Matches(){
        </div>
         <div id="maincontainer">
             <p id="matchesheader">Your Matches</p>
-            
         </div>
-        
+        <div>
+        SimpleAccordion(); 
+        </div>
         </>
     );
+
 }
 
 export default Matches;
