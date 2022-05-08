f0f06e37a0278a44f3bd280ff5fa798b743d5724
Diya Pathak <dpathak@usc.edu>
2022-05-08 16:19:41 -0700

    Accordion implemented

--- Frontend/src/Components/Matches/Matches.js
+++ Frontend/src/Components/Matches/Matches.js
@@ -113,7 +113,21 @@ function Matches(){
             <p id="matchesheader">Your Matches</p>
         </div>
         <div>
-        SimpleAccordion(); 
+        <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
         </div>
         </>
     );
