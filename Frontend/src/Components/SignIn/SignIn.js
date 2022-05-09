7346f5bba9cf070d3cdb88557e8c934f1a740d32
Jullie Lee <jullie@usc.edu>
2022-05-08 21:50:33 -0700

    fix

--- Frontend/src/Components/SignIn/SignIn.js
+++ Frontend/src/Components/SignIn/SignIn.js
@@ -1,115 +1,69 @@
 import * as React from 'react';
 import { Routes, Route, Link, useNavigate, Navigate} from "react-router-dom";
-// jullie edits (useRef)
-import { useRef, useState, useEffect, useContext} from 'react';
+import { useState, useEffect } from 'react';
 import logo from './../../../src/wavelength.svg';
 import Button from '@mui/material/Button';
 import '../SignIn/SignIn.css'
-import { InputSharp, SettingsInputSvideo, SettingsPowerRounded, SettingsSuggestRounded } from '@mui/icons-material';
-//jullie edits
-import axios from "axios"
-import AuthContext from "../../Context/AuthProvider";
+import { InputSharp, SettingsInputSvideo } from '@mui/icons-material';
 
-const LOGIN_URL = '/signin'
+const defaultvalues = {
+    email:"",
+    password:"",
+};
+
 
 function SignIn(){
-    const {setAuth} = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
-
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('')
-    const [errors, setError] = useState('');
+    const [inputs, setInputs] = useState(defaultvalues);
+    const [errors, setErrors] = useState({});
     const [isSubmit, setIsSubmit] = useState(false);
 
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
-
-    useEffect(() => {
-        setError('');
-    }, [user, pwd])
-
-    // const handleChange = (e) => {
-    //     const name = e.target.name;
-    //     const value = e.target.value;
-    //     setInputs(values => ({...values, [name]:value}) )
+    
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]:value}) )
         
-    // }
+    }
 
     const navigate = useNavigate();
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await axios.post(LOGIN_URL, JSON.stringify({user, pwd}),
-                {
-                    headers: {'Content-Type': 'application/json'},
-                    withCredentials: true
-                }
-            );
-
-            const accessToken = response?.data?.accessToken;
-            const roles = response?.data?.roles;
-            setAuth({user, pwd, roles, accessToken});
-            setUser('');
-            setPwd('');
-            setIsSubmit(true);
-        } catch (error) {
-            const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-            if (!user){
-                setError("Email is required!");
-            }
-            else if (!regex.test(user)){
-                setError("Invalid email format!");
-            }
-            else if (!pwd){
-                setError("Password is required!");
-            }
-            else if (pwd.length < 4){
-                setError("Password must be more than 4 characters.");
-            }
-
-            else if (errRef.response?.status === 401) {
-                setError("Unathuorized");
-            }
-            else {
-                setError("Unathuorized");
-            }
-            errRef.current.focus();
-        }
+    const handleSubmit = (event) => {
+        
+        event.preventDefault();
+        // console.log(inputs);
+        setErrors(validate(inputs));
+        setIsSubmit(true);
+        
     }
 
     useEffect(()=>{
         console.log(errors);
         if(Object.keys(errors).length === 0 && isSubmit){
+            console.log(inputs);
             return navigate('/discover');
         }
     }, [errors])
     
-    // const validate = (values) =>
-    // {
-    //     const all_errors = {};
+    const validate = (values) =>
+    {
+        const all_errors = {};
 
-    //     const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-    //     if (!values.email){
-    //         all_errors.email = "Email is required!";
-    //     }
-    //     else if (!regex.test(values.email)){
-    //         all_errors.email="Invalid email format!";
-    //     }
-    //     if (!values.password){
-    //         all_errors.password = "Password is required!";
-    //     }
-    //     else if (values.password.length < 4){
-    //         all_errors.password = "Password must be more than 4 characters."
-    //     }
+        if (!values.email){
+            all_errors.email = "Email is required!";
+        }
+        else if (!regex.test(values.email)){
+            all_errors.email="Invalid email format!";
+        }
+        if (!values.password){
+            all_errors.password = "Password is required!";
+        }
+        else if (values.password.length < 4){
+            all_errors.password = "Password must be more than 4 characters."
+        }
 
-    //     return all_errors;
-    // };
+        return all_errors;
+    };
 
     
 
@@ -119,7 +73,7 @@ function SignIn(){
            
             <ul>
             <li>
-                <Link to="/signUp" style={{ textDecoration: 'none', color:'black' }}>Sign Up</Link>
+                <Link to="/signup" style={{ textDecoration: 'none', color:'black' }}>Sign Up</Link>
             </li>
             <li id="logo_li">
                 <Link to="/" style={{ textDecoration: 'none' }}>
@@ -139,16 +93,16 @@ function SignIn(){
                 <form onSubmit={handleSubmit} >
                 <label >
                     <p>Email</p>
-                    <input type="text" name= "email" ref={userRef} value={user} onChange={(e) => setUser(e.target.value)} />
+                    <input type="text" name= "email" value={inputs.email} onChange={handleChange} />
                 </label>
-                <p className="errormsg2">{errors.email }</p>
+                <p className="errormsg">{errors.email }</p>
                 <label>
                     <p>Password</p>
-                    <input type="password" name= "password" value={pwd} onChange={(e) => setPwd(e.target.value)} />
+                    <input type="password" name= "password" value={inputs.password} onChange={handleChange} />
                 </label>
-                <p className= "errormsg2">{errors.password }</p>
+                <p className= "errormsg">{errors.password }</p>
                 <div>
-                    <Button type="submit" id="submitbutton" variant="contained" color="primary">Sign in</Button>
+                    <Button type="submit" id="submitbutton" variant="contained" color="primary"> Submit</Button>
                 </div>
                 </form>
             </div>
